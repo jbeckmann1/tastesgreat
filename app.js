c226688fdde65333f0a920b8c10abe0c14061de3
Johannes Beckmann <jbeckmannvcm@gmail.com>
2022-04-20 16:15:49 +0200

    change console log when server started to dynamic port

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(flash());
 app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-
-app.listen(3000, function() {
-	console.log("Server startet auf Port 3000")
+const port =process.env.PORT || 3000
+app.listen(port, function() {
+	console.log(`Server startet auf Port ${port}`)
 });
